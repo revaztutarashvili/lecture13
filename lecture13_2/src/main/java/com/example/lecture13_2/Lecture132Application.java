42b64f5f87cd12fa3377519692e3d7a44c616c7a
revazt <generationofjeans@gmail.com>
2025-04-29 23:55:53 +0400

    add lesson13.2

---
+++ lecture13_2/src/main/java/com/example/lecture13_2/Lecture132Application.java
@@ -0,0 +1,13 @@
+package com.example.lecture13_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture132Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture132Application.class, args);
+	}
+
+}
