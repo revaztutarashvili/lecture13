38fed8ae8bbaa4c5f063aa20d6626d486eba2629
revazt <generationofjeans@gmail.com>
2025-04-30 00:42:52 +0400

    add setter and getter for person class

---
+++ lecture13_2/src/main/java/com/example/lecture13_2/controller/PersonController.java
@@ -0,0 +1,8 @@
+package com.example.lecture13_2.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+    public 
+}
--- lecture13_2/src/main/java/com/example/lecture13_2/model/Person.java
+++ lecture13_2/src/main/java/com/example/lecture13_2/model/Person.java
@@ -1,4 +1,28 @@
 package com.example.lecture13_2.model;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Person {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    private String birthDate;
+
 }
